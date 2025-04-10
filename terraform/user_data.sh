a7d3a04ffe15fb30bc9705ecf98d2672e05b2a9f
takeru <tata.5593@gmail.com>
2025-04-10 20:30:14 +0900

    add reacrt

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+# 必要なパッケージをインストール
 yum update -y
-yum install -y python3 git
+yum install -y python3 git nodejs gcc-c++ make
+
+# Python パッケージインストール
 pip3 install --upgrade pip
 pip3 install flask gunicorn
 
+# アプリ用ディレクトリを作成して移動
 mkdir -p /home/ec2-user/app
 cd /home/ec2-user/app
 
-echo 'from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello from Flask on EC2!"' > app.py
+# GitHub からプロジェクトをクローン（★ここにあなたのURLを入れる）
+git clone https://github.com/takeru403/Ipoca_network
+# または ssh 方式でもOK: git@github.com:takeru/ipoca_network.git
 
+# React のビルド（frontend ディレクトリがある前提）
+cd frontend
+npm install
+npm run build
+cd ..
+
+# React ビルドファイルを Flask にコピー（static / templates ディレクトリへ）
+mkdir -p app/static app/templates
+cp -r frontend/build/static/* app/static/
+cp frontend/build/index.html app/templates/
+
+# Flaskアプリ起動
+cd app
 gunicorn -b 0.0.0.0:80 app:app --daemon
-
